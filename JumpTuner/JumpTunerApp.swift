49b728e64d4fcbcf4e3b74bbd3bdff54545c93a8
Jeff Ligon <jtligon@gmail.com>
2026-04-25 21:05:45 -0400

    Initial Commit

---
+++ JumpTuner/JumpTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpTunerApp.swift
+//  JumpTuner
+//
+//  Created by Jeff Ligon on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct JumpTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
